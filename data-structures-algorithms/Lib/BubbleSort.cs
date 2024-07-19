55a210dbb1af03fd982e5d3b3690f16f4e1a5d61
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2024-07-18 17:44:58 -0700

    Argument checking

--- data-structures-algorithms/Lib/BubbleSort.cs
+++ data-structures-algorithms/Lib/BubbleSort.cs
@@ -1,9 +1,16 @@
-﻿namespace Lib;
+namespace Lib;
 
 public class BubbleSort : ISortStrategy
 {
     public void Sort(int[] input)
     {
+        ArgumentNullException.ThrowIfNull(input);
+
+        if (input.Length <= 1)
+        {
+            return;
+        }
+
         for (var i = 0; i < input.Length; i++)
         {
             for (var j = 0; j < input.Length - i - 1; j++)
--- data-structures-algorithms/Test/BubbleSortTests.cs
+++ data-structures-algorithms/Test/BubbleSortTests.cs
@@ -10,6 +10,7 @@ public class BubbleSortTests
     [InlineData(new[] { 64 })]
     [InlineData(new[] { 64, 12 })]
     [InlineData(new[] { 1, 2, 3, 4, 5, 6})]
+    [InlineData(new int[] {})]
     public void Sort_Sorts_Array_In_Place(int[] input)
     {
         var copy = new int[input.Length];
@@ -18,5 +19,13 @@ public class BubbleSortTests
         new BubbleSort().Sort(input);
 
         copy.AsEnumerable().Order().Should().Equal(input);
+    }
+
+    [Fact]
+    public void Sort_When_Input_Null_Throws_Exception()
+    {
+        var act = () => new BubbleSort().Sort(null!);
+
+        act.Should().Throw<ArgumentNullException>();
     }
 }
