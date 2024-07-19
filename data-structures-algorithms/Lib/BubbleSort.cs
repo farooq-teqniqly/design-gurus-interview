82c5fcf37a672ef415ec382b134f52f451a6c552
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2024-07-18 18:28:22 -0700

    Quicksort

--- data-structures-algorithms/Lib/BubbleSort.cs
+++ data-structures-algorithms/Lib/BubbleSort.cs
@@ -1,4 +1,69 @@
+using System.Collections.Concurrent;
+using System.Threading.Tasks.Sources;
+
 namespace Lib;
+
+public class QuickSort : ISortStrategy
+{
+    public void Sort(int[] input)
+    {
+        if (input.Length <= 1)
+        {
+            return;
+        }
+
+        QuickSortRecursive(input, 0, input.Length - 1);
+
+    }
+
+    private void QuickSortRecursive(int[] input, int left, int right)
+    {
+        if (right - left <= 0)
+        {
+            return;
+        }
+
+        var pivot = input[right];
+        var partition = Partition(input, left, right, pivot);
+        QuickSortRecursive(input, left, partition - 1);
+        QuickSortRecursive(input, partition + 1, right);
+    }
+
+    private int Partition(int[] input, int left, int right, int pivot)
+    {
+        var leftPointer = left - 1;
+        var rightPointer = right;
+
+        while (true)
+        {
+            while (input[++leftPointer] < pivot)
+            {
+                ;
+            }
+
+            while (rightPointer > 0 && input[--rightPointer] > pivot)
+            {
+                ;
+            }
+
+            if (leftPointer >= rightPointer)
+            {
+                break;
+            }
+
+            Swap(input, leftPointer, rightPointer);
+        }
+
+        Swap(input, leftPointer, right);
+
+        return leftPointer;
+    }
+
+    private void Swap(int[] input, int x, int y)
+    {
+        (input[x], input[y]) = (input[y], input[x]);
+    }
+}
 
 public class BubbleSort : ISortStrategy
 {
--- data-structures-algorithms/Test/BubbleSortTests.cs
+++ data-structures-algorithms/Test/BubbleSortTests.cs
@@ -3,6 +3,24 @@ using Lib;
 
 namespace Test;
 
+public class QuickSortTests
+{
+    [Theory]
+    [InlineData(new[] { 64, 34, 25, 12, 22, 11, 90 })]
+    [InlineData(new[] { 64 })]
+    [InlineData(new[] { 64, 12 })]
+    [InlineData(new[] { 1, 2, 3, 4, 5, 6 })]
+    [InlineData(new int[] { })]
+    public void Sort_Sorts_Array_In_Place(int[] input)
+    {
+        var copy = new int[input.Length];
+        Array.Copy(input, copy, input.Length);
+
+        new QuickSort().Sort(input);
+
+        copy.AsEnumerable().Order().Should().Equal(input);
+    }
+}
 public class BubbleSortTests
 {
     [Theory]
