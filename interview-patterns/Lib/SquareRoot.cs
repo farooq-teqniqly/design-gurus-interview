c922768d8301faee9b08e13dc5e3965af301a209
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2024-07-22 11:23:45 -0700

    Binary search - calculate square root.

---
+++ interview-patterns/Lib/SquareRoot.cs
@@ -0,0 +1,37 @@
+namespace Lib;
+public class SquareRoot
+{
+    public static int Calculate(int x)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(x);
+
+        if (x < 2)
+        {
+            return x;
+        }
+
+        var left = 2;
+        var right = x / 2;
+
+        while (left <= right)
+        {
+            var pivot = left + ((right - left) / 2);
+            long guess = pivot * pivot;
+
+            if (guess < x)
+            {
+                left = pivot + 1;
+            }
+            else if (guess > x)
+            {
+                right = pivot - 1;
+            }
+            else
+            {
+                return pivot;
+            }
+        }
+
+        return right;
+    }
+}
---
+++ interview-patterns/Test/SquareRootTests.cs
@@ -0,0 +1,25 @@
+using FluentAssertions;
+using Lib;
+
+namespace Test;
+public class SquareRootTests
+{
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(1, 1)]
+    [InlineData(4, 2)]
+    [InlineData(8, 2)]
+    [InlineData(2, 1)]
+    public void Returns_Expected_Result(int num, int expectedSquareRoot)
+    {
+        SquareRoot.Calculate(num).Should().Be(expectedSquareRoot);
+    }
+
+    [Fact]
+    public void Throws_When_Input_Negative()
+    {
+        var act = () => SquareRoot.Calculate(-1);
+
+        act.Should().Throw<ArgumentOutOfRangeException>();
+    }
+}
